d8946985e00b6046c0cd970e48e9d462d74d4e4b
mohamedresk23 <mohammedresk4@gmail.com>
2024-07-26 23:47:46 +0300

    responce

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const shortenUrl = async () => {
   if (response.ok) {
     const data = await response.text();
     document.querySelector('.shorten-result').innerHTML += `
-    <div class="shorturl d-flex justify-content-between align-items-center bg-white p-1 rounded-1"> <span class="main-url ">${ Url }</span> 
+    <div class="shorturl d-flex flex-column flex-md-row justify-content-between align-items-center bg-white p-1 rounded-1 mb-5"> <span class="main-url ">${ Url }</span> 
     <div><span class="shortUrl--span">${ data }</span> <button class="btn btn-primary m-3 " onclick="copyBtn()">Copy</button> </div>
      </div>
     `;
@@ -17,6 +17,6 @@ const shortenUrl = async () => {
 };
 
 const copyBtn = () => {
-  var copyText = document.querySelector(".shortUrl").textContent;
+  var copyText = document.querySelector(".shortUrl--span").textContent;
   navigator.clipboard.writeText(copyText);
 };
